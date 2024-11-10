93ebe30df7b0789184f3ee2edcfe8cf33dfc71bb
AresFell <sarapapsova@gmail.com>
2024-11-10 10:34:33 +0100

    Create a update lokacie

---
+++ AGILE2024_BE/Models/Response/LocationResponse.cs
@@ -0,0 +1,18 @@
+﻿namespace AGILE2024_BE.Models.Response
+{
+    public class LocationResponse
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public string City { get; set; }
+        public string ZipCode { get; set; }
+        //širka
+        public double? Latitude { get; set; }
+        //dlžka
+        public double? Longitude { get; set; }
+        public DateTime LastEdited { get; set; }
+        public DateTime Created { get; set; }
+        public bool Archived { get; set; }
+    }
+}
