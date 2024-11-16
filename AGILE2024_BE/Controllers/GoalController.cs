db7b7665c3076bdfac5b48e715f1fa1376a171eb
Alexandra Vojtasová <avojtasova33@gmail.com>
2024-11-16 08:09:28 +0100

    goal controller

---
+++ AGILE2024_BE/Controllers/GoalController.cs
@@ -0,0 +1,26 @@
+﻿using AGILE2024_BE.Data;
+using AGILE2024_BE.Models.Identity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AGILE2024_BE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GoalController : ControllerBase
+    {
+        private UserManager<ExtendedIdentityUser> userManager;
+        private RoleManager<IdentityRole> roleManager;
+        private IConfiguration config;
+        private AgileDBContext dbContext;
+
+        public GoalController(UserManager<ExtendedIdentityUser> um, IConfiguration co)
+        {
+            this.userManager = um;
+            this.config = co;
+            this.roleManager = roleManager;
+            this.dbContext = dbContext;
+        }
+    }
+}
