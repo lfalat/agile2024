f0b5c9673c904b69a278ef18b9b687ac06cfc75d
alexandravoj <avojtasova33@gmail.com>
2024-11-20 21:53:59 +0100

    pridanie zamestnaneckej karty

--- AGILE2024_BE/Controllers/EmployeeCardController.cs
+++ AGILE2024_BE/Controllers/EmployeeCardController.cs
@@ -1,17 +1,18 @@
 ﻿using AGILE2024_BE.Data;
-using AGILE2024_BE.Models;
 using AGILE2024_BE.Models.Identity;
+using AGILE2024_BE.Models.Requests.EmployeeCardRequests;
+using AGILE2024_BE.Models.Response;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 
 namespace AGILE2024_BE.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    [Authorize(Roles = RolesDef.Spravca)]
     public class EmployeeCardController : ControllerBase
     {
         private UserManager<ExtendedIdentityUser> userManager;
@@ -25,6 +26,87 @@ namespace AGILE2024_BE.Controllers
             this.config = co;
             this.roleManager = rm;
             this.dbContext = db;
+        }
+
+        [HttpGet("EmployeeCards")]
+        [Authorize(Roles = RolesDef.Veduci)]
+        public async Task<IActionResult> EmployeeCards()
+
+
+        {
+            ExtendedIdentityUser? user = await userManager.FindByEmailAsync(User.Identity?.Name!);
+            var superiorId = user.Id;
+
+            if (string.IsNullOrEmpty(superiorId))
+            {
+                return Unauthorized("Nedokázali sme získať informácie o prihlásenom používateľovi.");
+            }
+            // Získanie zamestnancov, ktorí majú rolu zamestnanec a patria do oddelenia so zadaným superiorId
+            var employeeCards = await dbContext.EmployeeCards
+            .Include(ec => ec.User)
+            .Where(ec => ec.Department != null)
+            .Join(
+                dbContext.UserRoles,
+                ec => ec.User.Id,
+                ur => ur.UserId,
+                (ec, ur) => new { EmployeeCard = ec, UserRole = ur }
+            )
+            .Join(
+                dbContext.Roles,
+                combined => combined.UserRole.RoleId,
+                r => r.Id,
+                (combined, role) => new { EmployeeCard = combined.EmployeeCard, Role = role, Department = combined.EmployeeCard.Department }
+            )
+            .Where(joined => joined.Role.Name == RolesDef.Zamestnanec)
+            //.Where(joined => joined.Department.Superior.id == superiorId.ToString()) // Filtrovanie podľa SuperiorId v Department
+            .Select(joined => new EmployeeCardResponse
+            {
+                EmployeeId = joined.EmployeeCard.Id,
+                Email = joined.EmployeeCard.User.Email,
+                Name = joined.EmployeeCard.User.Name ?? string.Empty,
+                TitleBefore = joined.EmployeeCard.User.Title_before ?? string.Empty,
+                TitleAfter = joined.EmployeeCard.User.Title_after ?? string.Empty,
+                Department = joined.Department.Name ?? "N/A",
+                Surname = joined.EmployeeCard.User.Surname ?? string.Empty,
+                MiddleName = joined.EmployeeCard.User.MiddleName
+            })
+                .ToListAsync();
+
+            return Ok(employeeCards);
+        }
+
+        [HttpPost("Update")]
+        [Authorize(Roles = RolesDef.Spravca)]
+        public async Task<IActionResult> Update([FromBody] UpdateEmployeeCardRequest employeeCardRequest)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var user = await dbContext.Users.FindAsync(employeeCardRequest.UserId);
+            var employeeCard = await dbContext.EmployeeCards
+                .FindAsync(employeeCardRequest.Id);
+
+            var level = await dbContext.Levels.FindAsync(employeeCardRequest.Level);
+            var location = await dbContext.Locations.FindAsync(employeeCardRequest.Location);
+            var department = await dbContext.Departments.FindAsync(employeeCardRequest.Department);
+            var contractType = await dbContext.ContractTypes.FindAsync(employeeCardRequest.ContractType);
+
+            employeeCard.Birthdate = DateTime.Parse(employeeCardRequest.Birth);
+            employeeCard.ContractType = contractType;
+            employeeCard.Location = location;
+            employeeCard.Department = department;
+            employeeCard.WorkPercentage = employeeCardRequest.WorkTime;
+            employeeCard.Level = level;
+            employeeCard.StartWorkDate = DateTime.Parse(employeeCardRequest.StartWorkDate);
+            employeeCard.LastEdited = DateTime.Now;
+
+            dbContext.EmployeeCards.Update(employeeCard);
+
+            await dbContext.SaveChangesAsync();
+
+            return Ok();
         }
     }
 }
